b80b4b6a31ab3607553921db5684304e47c4ef26
Boris Nagaev <bnagaev@gmail.com>
2018-05-20 23:36:35 +0200

    batch random to speed up

--- main.go
+++ main.go
@@ -50,19 +50,52 @@ func main() {
 		fmt.Println(strings.Join(parts, " "))
 	}
 
+	randBuf := make([]byte, 1024)
+	randCurByte := 1024
+	randCurBit := uint(0)
+
+	randBit := func() int {
+		if randCurByte == 1024 {
+			fastrand.Read(randBuf)
+			randCurByte = 0
+			randCurBit = 0
+		}
+		r := (randBuf[randCurByte] >> randCurBit) & 1
+		randCurBit++
+		if randCurBit == 8 {
+			randCurBit = 0
+			randCurByte++
+		}
+		return int(r)
+	}
+
+	randIntn := func(n int) int {
+	start:
+		max := n - 1
+		r := 0
+		for max != 0 {
+			r = (r << 1) | randBit()
+			max = max >> 1
+		}
+		if r >= n {
+			goto start
+		}
+		return r
+	}
+
 	for i := 0; ; i++ {
 		if i%*period == 0 {
 			printScores()
 		}
 
 		// Choose two random players.
-		p1 := fastrand.Intn(remaining)
-		p2 := fastrand.Intn(remaining - 1)
+		p1 := randIntn(remaining)
+		p2 := randIntn(remaining - 1)
 		if p2 >= p1 {
 			p2++
 		}
 		var loser, winner int
-		if fastrand.Intn(2) == 0 {
+		if randBit() == 0 {
 			loser = p1
 			winner = p2
 		} else {
